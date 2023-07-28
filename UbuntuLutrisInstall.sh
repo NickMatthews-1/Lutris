9bf56c6f8cd6e1eb01f4b1b940737328a7c0fb49
Nick Matthews <matthews3102000@gmail.com>
2023-07-28 22:24:01 +0100

    Added Scripts
    Added the Ubuntu Install and Uninstall scripts for Lutris.

---
+++ UbuntuLutrisInstall.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# This updates your system
+sudo apt update
+
+# This upgrades your system
+sudo apt upgrade
+
+# This downloads the deb package needed from GitHub
+wget https://github.com/lutris/lutris/releases/download/v0.5.13/lutris_0.5.13_all.deb
+
+# This installs the deb package for Lutris
+sudo dpkg -i lutris_0.5.13_all.deb
+
+# This updates your system
+sudo apt update
+
+# This upgrades your system
+sudo apt upgrade
+
+# When you use Linux don't Panic!
+# Copright Nick Matthews 2023 GPLv3 
---
+++ UbuntuLutrisUninstall.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# This updates your system
+sudo apt update
+
+# This upgrades your system
+sudo apt upgrade
+
+# This uninstalls Lutris
+sudo dpkg -r lutris
+
+# This updates your system
+sudo apt update
+
+# This upgrades your system
+sudo apt upgrade
+
+# When you use Linux don't Panic!
+# Copright Nick Matthews 2023 GPLv3 
