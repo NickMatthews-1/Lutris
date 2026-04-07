c1a6c9f6867435f93f6d5b780391e5e943bb2e18
Nick Arch Linux <linuxpanic1@gmail.com>
2026-04-07 12:20:33 +1000

    Updated for Latest Install

--- LutrisInstall.sh
+++ LutrisInstall.sh
@@ -7,10 +7,10 @@ sudo apt update
 sudo apt upgrade
 
 # This downloads the deb package needed from GitHub
-wget https://github.com/lutris/lutris/releases/download/v0.5.18/lutris_0.5.18_all.deb
+wget https://github.com/lutris/lutris/releases/download/v0.5.22/lutris_0.5.22_all.deb
 
 # This installs the deb package for Lutris
-sudo dpkg -i lutris_0.5.18_all.deb
+sudo dpkg -i lutris_0.5.22_all.deb
 
 # This updates your system
 sudo apt update
@@ -19,4 +19,4 @@ sudo apt update
 sudo apt upgrade
 
 # When you use Linux don't Panic!
-# Copright Nick Matthews 2024 GPLv3 
+# Copright Nick Matthews 2026 GPLv3 
